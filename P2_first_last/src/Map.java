e931e5bc06ab46bcddf4851bcf1c97d2a5881c90
1916396 <1916396@01-18115.pusd.dom>
2025-03-11 14:05:54 -0700

    Day 2

---
+++ P2_first_last/src/Map.java
@@ -0,0 +1,3 @@
+public class Map {
+	private Tile[][] map;
+}
--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,8 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
+		readMap("Map1");
+		
+	}
+	
+	public static void readMap(String filename) {
+		
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows  = scanner.nextInt();
+			int numCols  = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+
+		}catch(FileNotFoundException e) {
+			//handle exception
+			System.out.println(e);
+		}
+		
 	}
 
 }
